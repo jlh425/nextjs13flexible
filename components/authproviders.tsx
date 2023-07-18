2cea17e41b91e9736ff0dd8c68b5e9258a7b3081
jlh425 <105890316+jlh425@users.noreply.github.com>
2023-07-18 06:51:44 -0600

    update authproviders

--- components/authproviders.tsx
+++ components/authproviders.tsx
@@ -1,17 +1,21 @@
 "use client"
 
-import {getProviders, signIn} from 'next-auth/react'
-import { useState, useEffect } from 'react'
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+
+import Button from './Button';
 
 type Provider = {
     id: string;
     name: string;
     type: string;
-    singinUrl: string;
+    signinUrl: string;
     callbackUrl: string;
-    singinUrlParams?: Record<string, string> | null;
-}
-type Providers = Record<string, Provider>;
+    signinUrlParams?: Record<string, string> | undefined;
+  };
+  
+  type Providers = Record<string, Provider>;
+
 
 const AuthProviders = () => {
     const [providers, setProviders] = useState<Providers | null>(null);
@@ -19,20 +23,22 @@ const AuthProviders = () => {
     useEffect(() => {
         const fetchProviders = async () => {
             const res = await getProviders();
+    
             setProviders(res);
         }
+
         fetchProviders();
-    },[]);
-    
-  if(providers){
-    return (
-        <div>
-            {Object.values(providers).map((provider: Provider, () => (
-                <button key={i} onClick={() => signIn(provider?.id)}>{Providers.id}</button>
-            ))}
-        </div>
-    )
-  }
+    }, []);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
+                ))}
+            </div>
+        )
+    }
 }
 
 export default AuthProviders
